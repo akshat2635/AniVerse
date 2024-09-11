2719603b0a06e2552b73a2174c1ee8494a9216ab
akshat2635 <akshatjain2635@gmail.com>
2024-09-11 14:28:53 +0530

    On branch master Your branch is up to date with 'origin/master'.
    Changes to be committed:
    	modified:   website/src/components/login.js

--- website/src/components/login.js
+++ website/src/components/login.js
@@ -145,7 +145,7 @@ export default function LoginForm() {
             </form>
             </div>
             <div className="px-8 py-4 bg-gray-700 text-center">
-            <span className="text-gray-400">Don't have an account?</span>
+            <span className="text-gray-400">Don&apos;t have an account?</span>
             <a className="font-medium text-indigo-500 hover:text-indigo-400" href="/register">Sign up</a>
             </div>
         </div>
